018685b2fd3d7df838217242faa25003888d39c6
Jonathan Dorety <jdorety@flur.ee>
2020-06-04 11:27:45 -0700

    add some jsdoc comments

--- src/flureeTransact.js
+++ src/flureeTransact.js
@@ -1,9 +1,15 @@
 import axios from "axios";
+import { string } from "prop-types";
 
 const configAxios = (database) => axios.create({
   baseURL: `http://localhost:8080/fdb/${database}/transact`
 })
 
+/**
+ * 
+ * @param {string} database A string indicating the fluree database to be transacted upon
+ * @param {Array} transactions Array of JSON objects containing Fluree transactions
+ */
 export const flureeTransact = (database, transactions) => {
   const ax = configAxios(database);
   return ax.post("", transactions).then(res => res).catch(err => err);
